a5ef1b862d0a374c45cbfe52cedd97b14f2f8143
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-07-07 19:37:37 +0600

    Number Input
    Just inputted two numbers one is an integer and another is a float number, then multiply them.

---
+++ number_input.py
@@ -0,0 +1,22 @@
+"""
+PROBLEM 1:
+Take two inputs from the user. One will be an integer. The other will be a float number.
+Then multiply them to display the output.
+
+HINTS:
+Input two numbers one is integer and another one is float. Then multiply them .
+
+"""
+
+# here is the two inputs............
+number1 = int(input('First input is : '))
+number2 = float(input('Second input is : '))
+
+# store the result ................
+
+result = number1 * number2
+
+# display the result...............
+
+print('The multiplication of two numbers is : ', result)
+
