1f33acc6289d322a9e950b6e39185a505159b7e2
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-07-15 12:36:45 +0600

    Max_3
    max number out of three.

---
+++ max_3.py
@@ -0,0 +1,19 @@
+"""
+Problem 7:
+Find the max one among three.
+
+"""
+num1 = int(input("First number: "))
+num2 = int(input("Second number: "))
+num3 = int(input("Third number: "))
+
+largest = num1
+
+if (num2 >= num1) and (num2 >= num3):
+    largest = num2
+elif (num3 >= num1) and (num3 >= num2):
+    largest = num3
+else:
+    largest = num1
+
+print("Largest number you entered is: ", largest)
